3cd86ec977c1aa768ca77cbba57284552856af5b
元气卓然 <liuzhuoran41@163.com>
2017-02-25 16:43:57 +0800

    初始化
    1.初始化Mac版本
    2.初始化Ignore文件

---
+++ Xcode/RemoteLabMac/RemoteLabMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RemoteLabMac
+//
+//  Created by zhuoran liu on 2017/2/25.
+//  Copyright © 2017年 zhuoran liu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode/RemoteLabMac/RemoteLabMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RemoteLabMac
+//
+//  Created by zhuoran liu on 2017/2/25.
+//  Copyright © 2017年 zhuoran liu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
